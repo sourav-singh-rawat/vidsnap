0b2f128e64878dd039546694e04f499f35a05c49
sourav-singh-rawat <souravrawat1999@gmail.com>
2024-09-20 13:29:03 +0530

    Add features in camera modules

---
+++ lib/modules/data/camera/camera.dart
@@ -0,0 +1,137 @@
+import 'dart:developer';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:vidsnap/modules/domain/camera/camera.dart';
+
+part 'utils/helper.dart';
+
+class AppCameraImpl implements AppCamera {
+  bool isInitialized = false;
+  List<CameraDescription>? deviceAvailableCameras;
+  CameraController? controller;
+  CameraLensDirection? selectedCameraLens;
+
+  @override
+  Future<void> bootUp() {
+    // TODO: implement bootUp
+    throw UnimplementedError();
+  }
+
+  @override
+  void onBootUp() {
+    // TODO: implement onBootUp
+  }
+
+  @override
+  void bootDown() {
+    // TODO: implement bootDown
+  }
+
+  @override
+  Future<bool> initialize() async {
+    try {
+      if (controller != null) {
+        return true;
+      }
+
+      deviceAvailableCameras = await _availableCameras();
+      if (deviceAvailableCameras != null && deviceAvailableCameras!.isNotEmpty) {
+        controller = await _initializeCameraController(deviceAvailableCameras![0]);
+
+        selectedCameraLens = deviceAvailableCameras![0].lensDirection;
+
+        isInitialized = controller?.value.isInitialized ?? false;
+      }
+
+      return isInitialized;
+    } catch (error) {
+      log("[AppCamera][_availableCameras]: Error");
+      rethrow;
+    }
+  }
+
+  Future<List<CameraDescription>> _availableCameras() async {
+    try {
+      final cameras = await availableCameras();
+      return cameras;
+    } catch (error) {
+      log("[AppCamera][_availableCameras]: Error: $error");
+      throw ErrorDescription('Opps, Failed to detect camera on device');
+    }
+  }
+
+  Future<CameraController> _initializeCameraController(CameraDescription camera) async {
+    try {
+      final controller = CameraController(deviceAvailableCameras![0], ResolutionPreset.max);
+      await controller.initialize();
+
+      return controller;
+    } catch (error) {
+      log("[AppCamera][_initializeCameraController]: Error: $error");
+      if (error is CameraException) {
+        throw ErrorDescription(error.description ?? 'Opps, Camera plugin exception, Try Again');
+      }
+      throw ErrorDescription('Opps, Something went wrong, Go Back and Try Again!');
+    }
+  }
+
+  @override
+  Future<void> switchCamera() async {
+    try {
+      if (deviceAvailableCameras == null) {
+        throw ErrorDescription('Failed to detect camera on device');
+      }
+
+      CameraLensDirection _selectedCameraLens = selectedCameraLens ?? CameraLensDirection.front;
+      if (selectedCameraLens == CameraLensDirection.front) {
+        _selectedCameraLens = CameraLensDirection.back;
+      } else {
+        _selectedCameraLens = CameraLensDirection.front;
+      }
+
+      controller = await _initializeCameraController(deviceAvailableCameras![_selectedCameraLens.index]);
+
+      selectedCameraLens = _selectedCameraLens;
+
+      isInitialized = controller?.value.isInitialized ?? false;
+    } catch (error) {
+      log("[AppCamera][switchCamera]: Error: $error");
+      rethrow;
+    }
+  }
+
+  Widget cameraPreview() {
+    if (controller == null) {
+      return const Offstage();
+    }
+
+    return CameraPreview(controller!);
+  }
+
+  @override
+  Future<void> startRecording() {
+    // TODO: implement startRecording
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> stopRecording() {
+    // TODO: implement stopRecording
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> dispose() async {
+    try {
+      await controller?.dispose();
+      controller = null;
+      deviceAvailableCameras = null;
+      selectedCameraLens = null;
+      isInitialized = false;
+    } catch (error) {
+      log("[AppCamera][dispose]: Error: $error");
+      rethrow;
+    }
+  }
+}
---
+++ lib/modules/domain/camera/camera.dart
@@ -0,0 +1,22 @@
+import 'package:vidsnap/modules/data/camera/camera.dart';
+import 'package:vidsnap/modules/domain/module.dart';
+
+abstract class AppCamera implements AppModule<void> {
+  AppCamera._();
+
+  static final AppCameraImpl instance = AppCameraImpl();
+
+  factory AppCamera() {
+    return instance;
+  }
+
+  Future<bool> initialize();
+
+  Future<void> switchCamera();
+
+  Future<void> startRecording();
+
+  Future<void> stopRecording();
+
+  Future<void> dispose();
+}
