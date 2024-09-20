f298d5459bdec07a6b76e5463637ef85da0ecfda
sourav-singh-rawat <souravrawat1999@gmail.com>
2024-09-20 13:28:20 +0530

    Add params in scaffold wrapper

--- lib/presentation/core_widgets/scaffold.dart
+++ lib/presentation/core_widgets/scaffold.dart
@@ -3,17 +3,31 @@ import 'package:flutter/material.dart';
 class AppScaffold extends StatelessWidget {
   final PreferredSizeWidget? appBar;
   final Widget? body;
+  final Widget? floatingActionButton;
+  final Color? backgroundColor;
+  final bool extendBodyBehindAppBar;
+  final Widget? bottomSheet;
   const AppScaffold({
     super.key,
     this.body,
     this.appBar,
+    this.floatingActionButton,
+    this.backgroundColor,
+    this.extendBodyBehindAppBar = false,
+    this.bottomSheet,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar,
-      body: body,
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: extendBodyBehindAppBar,
+        backgroundColor: backgroundColor,
+        appBar: appBar,
+        body: body,
+        floatingActionButton: floatingActionButton,
+        bottomSheet: bottomSheet,
+      ),
     );
   }
 }
