f60e26f0dd389ffa013dd2319b77454c2b75c519
sourav-singh-rawat <souravrawat1999@gmail.com>
2024-09-21 10:59:11 +0530

    Minor fix

--- lib/presentation/views/camera/view.dart
+++ lib/presentation/views/camera/view.dart
@@ -36,7 +36,7 @@ class _CameraViewState extends State<CameraView> with WidgetsBindingObserver {
     }
 
     if (state == AppLifecycleState.inactive) {
-      if (!AppCamera.instance.isInitialized) {
+      if (!AppRepository.camera.isInitialized) {
         return;
       }
 
