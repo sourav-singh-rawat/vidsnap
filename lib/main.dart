52dafb0a83f8dfbe4d57f3bc2033c3f4c8c83288
sourav-singh-rawat <souravrawat1999@gmail.com>
2024-09-22 14:09:40 +0530

    Remove all todos

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vidsnap/presentation/app.dart';
 
-///Add no video screen
-///Orientation capture and show
-///NullPointer Error
-///Add preview for recent saved
-///Preview Capture
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
